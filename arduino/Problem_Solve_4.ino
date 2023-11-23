1600ab0a1a29e38418b7f44bda5fc5ca63e71b0b
axckzz <144990882+axckzz@users.noreply.github.com>
2023-11-23 08:29:42 +0100

    Update Problem_Solve_4.ino

--- arduino/Problem_Solve_4.ino
+++ arduino/Problem_Solve_4.ino
@@ -1,16 +1,15 @@
 void setup() {
   Serial.begin(9600);
   for (int numeroProblema = 1; numeroProblema <= 100; ++numeroProblema) {
-      int lastDigit = numeroProblema % 10;
-      if (lastDigit == 6 || lastDigit == 3) {
+  if (numeroProblema % 10 == 3 || numeroProblema % 10 == 6) {
   resolverProblemaDibujo(numeroProblema);
+  }
  }
-}
 }
   void loop() {
   }
 
-  void resolverProblemaDibujo(int n) { 
-    Serial.print("Resolviendo problema = ");
-    Serial.println(n);
+  void resolverProblemaDibujo(int numeroProblema) { 
+  Serial.print("Resolviendo problema");
+  Serial.println(numeroProblema);
   }
