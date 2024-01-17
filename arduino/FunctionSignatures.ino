60782dbd376771b3ab69a8dabb7c118652263d90
axckzz <144990882+axckzz@users.noreply.github.com>
2024-01-17 10:38:25 +0100

    Add files via upload

---
+++ arduino/FunctionSignatures.ino
@@ -0,0 +1,33 @@
+//exercise 1
+double findTheLeg( double hypotenuse, double otherLeg) //1
+bool validateDNI(String x) //2
+bool createBigHouseInMinecraft() //3
+bool createBigHouseInMinecraft(IP server) //4
+PVector addPVector (PVector x, PVector y) //5
+void printFib (int n) //6
+void soundScale (int pin) //7
+void soundScale (int pin, bool chromatic) //8
+void soundScale ( bool chromatic, int pin) //9
+bool nextClassAlready(long milliseconds) //10
+int nextClassAlready(long milliseconds) //11
+int HTTPConnect(string URL) //12
+
+//exercise 2
+
+void startWithQuixote (string) //1
+void inverseNumber(int x) //2
+bool isItAGoodDay() //3
+double volumeCilinder(double radius, double height) //4
+bool isThereAnyBeetlejuice(const char* input) //5
+int notOver5(int number) //6
+void singLullaby() //7
+
+
+
+
+
+
+
+
+
+
