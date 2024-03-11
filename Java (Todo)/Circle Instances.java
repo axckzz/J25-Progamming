d8b37f494eeb6a5c2fe22b31b1e1c484752bd3b2
axckzz <144990882+axckzz@users.noreply.github.com>
2024-03-11 15:04:58 +0100

    Create Circle Instances.java

---
+++ Java (Todo)/Circle Instances.java
@@ -0,0 +1,9 @@
+Circle circle1 = new Circle();
+
+Circle c2 = new Circle(5.0);
+
+Circle c3 = new Circle(10.0, "green");
+
+Circle colinRobinson = new Circle(-2.0, "greenish");
+
+Circle circle5 = new Circle(1.0, "blue");
